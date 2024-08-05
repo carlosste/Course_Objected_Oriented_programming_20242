fee10f39b19e959952b7341251d3747fed8a3148
Carlos Steven Lozano <167788548+carlosste@users.noreply.github.com>
2024-08-05 15:51:55 +0000

    mi primer archivo en java

--- 20242/Ejecutar.java
+++ 20242/Ejecutar.java
@@ -1,5 +1,6 @@
 Public class Ejecutar {
-    public static void main(string[] args){
+    public static void main(String[] args){
         System.out.println("programación orientada a objetos");
     }
+
 }
