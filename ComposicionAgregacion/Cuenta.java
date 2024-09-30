72a72cd7fdbfa333931e1401fc72ee8b29d96f87
Carlos Steven Lozano <167788548+carlosste@users.noreply.github.com>
2024-09-30 15:03:27 +0000

    Composición agregación

---
+++ ComposicionAgregacion/Cuenta.java
@@ -0,0 +1,78 @@
+package ComposicionAgregacion;
+
+
+public class Cuenta {
+    private String nroCuenta;
+    private String tipoCuenta;
+    private Double saldo;
+
+    public Cuenta(String nroCuenta, String tipoCuenta, Double saldo) {
+        this.nroCuenta = nroCuenta;
+        this.tipoCuenta = tipoCuenta;
+        this.saldo = saldo;
+    }
+
+
+    public String getNroCuenta() {
+        return nroCuenta;
+    }
+
+
+    public void setNroCuenta(String nroCuenta) {
+        this.nroCuenta = nroCuenta;
+    }
+
+
+    public String getTipoCuenta() {
+        return tipoCuenta;
+    }
+
+
+    public void setTipoCuenta(String tipoCuenta) {
+        this.tipoCuenta = tipoCuenta;
+    }
+
+
+    public Double getSaldo() {
+        return saldo;
+    }
+
+
+    public void setSaldo(Double saldo) {
+        this.saldo = saldo;
+    }
+
+
+
+    @Override
+    public String toString() {
+        return "Cuenta [nroCuenta=" + nroCuenta + ", tipoCuenta=" + tipoCuenta + ", saldo=" + saldo + "]";
+    }
+
+
+    public boolean consignar(String nroCuenta, double monto ) {
+        boolean flag = false;
+        if (nroCuenta.equals("709876")) {
+            saldo += monto;
+            flag = true;
+        }
+        return flag;
+    }
+
+    public String consignarMsg(String nroCuenta, double monto ) {
+        String msg= " ";
+        if (nroCuenta.equals("709876")) {
+            saldo += monto;
+            msg= "Transaccion exitosa....  saldo actual:" + saldo;
+        }else{
+            msg= "Transaccion fallida....  saldo actual:" + saldo;
+        }
+        return msg;
+    }
+
+
+
+
+
+
+}
---
+++ ComposicionAgregacion/EjecutarComposicionAgregacion.java
@@ -0,0 +1,21 @@
+package ComposicionAgregacion;
+public class EjecutarComposicionAgregacion {
+
+    public static void main(String[] args) {
+
+
+        Cliente objCliente1 = new Cliente("1008909765", "pipe", "Perez");
+        Cuenta objCuenta1 = new Cuenta("709876", "Ahorros", 0.0);
+        Banco objBanco1 = new Banco("900345123", "Banco UCC" , objCuenta1);
+       
+        System.out.println(objBanco1);
+        System.out.println(objBanco1.esCliente(objCliente1));
+    
+        //System.out.println("Transaccion: " + objCuenta1.consignarMsg(objCuenta1.getNroCuenta(), 150000.0));
+        
+        System.out.println(objCuenta1.consignarMsg(objCuenta1.getNroCuenta(), 150000.0));
+        System.err.println(objCuenta1);
+    } 
+
+    
+}
