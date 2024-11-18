e7b82cf66cfabb5367c0855815c15dc2851915f3
Carlos Steven Lozano <167788548+carlosste@users.noreply.github.com>
2024-11-18 04:46:17 +0000

    Actividad final

--- JuegoLucha/JuegoLucha.java
+++ JuegoLucha/JuegoLucha.java
@@ -43,10 +43,6 @@ import java . util . Scanner ;
 
     // Metodo principal que ejecuta el juego
     public static void main ( String [] args ) {
-    Scanner scanner = new Scanner ( System . in ) ;
-
-    System . out . print (" Introduce el nombre del jugador 1:") ;
-    String nombre1 = scanner . nextLine () ;
 
     System . out . print (" Introduce el nombre del jugador 2:") ;
     String nombre2 = scanner . nextLine () ;
